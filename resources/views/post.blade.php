d08320ac0e4fc3901debe5cfdc29fda17dde120e
saranonearth <saran.suresh@outlook.com>
2020-10-09 20:01:00 +0530

    firebase storage integrated

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,7 +12,9 @@
 <script src="https://kit.fontawesome.com/e19e904283.js" crossorigin="anonymous"></script>
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
-
+<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-analytics.js"></script>
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;0,700;1,300;1,500&display=swap" rel="stylesheet">
 
@@ -56,47 +58,13 @@
         <div class="side-2">
             <div class="bar">
                 <div>
-                    <a class="link-b" href="/home">Home</a>
+                    <a class="link-b" href="/home"> <i class="fas fa-home"></i> Home</a>
                 </div>
                 <div class="ml">
-                    <a class="link-b" href="/posts/{{$user->id}}">My posts</a>
+                    <a class="link-b" href="/posts/{{$user->id}}"><i class="fas fa-list"></i> My posts</a>
                 </div>
             </div>
-            <div class="post-input">
-                <div>
-                    <form action="/posts" method="POST">
-                        @csrf
-                        <div class="cross">
-                            <div class="i-col">
-                                <div>
-                                    <textarea class="form-control" placeholder="Got anything in mind? Share!" name="postData" rows="3"></textarea>
-                                </div>
-                                <input type="hidden" name="userId" value="{{$user->id}}">
-                                <input type="hidden" name="userName" value="{{$user->name}}">
-                                <input type="hidden" name="userImage" value="{{$user->imageURL}}">
-                                <input type="hidden" name="imageURL" value="">
-                            </div>
 
-
-                            <div class="col-span">
-
-                                <div>
-
-
-                                    <input type="file" id="post-image-upload" capture="filesystem" name="post-image" accept="image/*" style="display: none;">
-                                    <input class="media-btn" type="button" value="Media" onclick="document.getElementById('post-image-upload').click();">
-
-                                </div>
-                                <div>
-                                    <button class="submit-btn" type="submit">Post <i class="fas fa-share"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-
-            </div>
 
 
             <div class="post-card width-c">
@@ -119,9 +87,11 @@
                 <div class="post-data">
                     <p>{{$postData->data}}</p>
                 </div>
+                @if($postData->imageURL)
                 <div>
-                    <img class="post-image" src="https://via.placeholder.com/150" alt="">
+                    <img class="post-image" src="{{$postData->imageURL}}" alt="{{$postData->imageURL}}">
                 </div>
+                @endif
 
                 <h1>Comments</h1>
                 <div>
@@ -168,6 +138,8 @@
         </div>
     </div>
 
+    <script src="/js/firebase.js"></script>
+    <script src="/js/fileupload.js"></script>
 </body>
 
 </html>
